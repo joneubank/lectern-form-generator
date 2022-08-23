b266edc6b31a10d3858760aef81554bdc3cbf367
joneubank <joneubank@gmail.com>
2022-08-23 13:49:46 -0400

    fix return type of validateRegex (boolean)

--- libraries/lectern/src/validation/restrictions/regex.ts
+++ libraries/lectern/src/validation/restrictions/regex.ts
@@ -1,5 +1,5 @@
 function validateRegex(value: string, regex: string) {
   const regExp = new RegExp(regex);
-  return value.match(regExp);
+  return !!value.match(regExp);
 }
 export default validateRegex;
