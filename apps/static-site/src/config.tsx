74f0e67a4423e125d87cdda3785484b846a8910a
joneubank <joneubank@gmail.com>
2022-08-23 13:47:20 -0400

    Remove unused env variable

--- apps/static-site/src/config.tsx
+++ apps/static-site/src/config.tsx
@@ -3,9 +3,5 @@ const config = {
     title: process.env.SITE_TITLE || 'Schema Generated Form',
     description: process.env.SITE_DESCRIPTION || 'Create a structured TSV through form submission',
   },
-  schema: {
-    url: process.env.SCHEMA_URL,
-    file: process.env.SCHEMA_FILE_PATH || '../../../resources/dictionaries/simple-dictionary.json',
-  },
 };
 export default config;
